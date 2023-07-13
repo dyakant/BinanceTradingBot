2db4c6baa09f2794b960689f1f70130509ab3ec1
Anton Dyakov <dyakant@gmail.com>
2023-07-13 18:06:07 +0300

    Into changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
 import codeExecution.RealTimeCommandOperator;
 import data.AccountBalance;
 import data.Config;
+import lombok.extern.slf4j.Slf4j;
 import singletonHelpers.BinanceInfo;
 import singletonHelpers.TelegramMessenger;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         Config config = new Config();
         AccountBalance accountBalance = AccountBalance.getAccountBalance();
         BinanceInfo binanceInfo = BinanceInfo.getBinanceInfo();
         RealTimeCommandOperator realTimeCommandOperator = new RealTimeCommandOperator();
-        TelegramMessenger.send("Binance Trading Bot is running...");
+        TelegramMessenger.send("Binance Trading Bot is running... Balance: " + AccountBalance.getAccountBalance().getCoinBalance("usdt"));
         System.out.println("Binance Trading Bot is running! Type commands.");
+//        log.info("Start");
+//        log.debug("Start debug");
         try {
             realTimeCommandOperator.run();
         } catch (InterruptedException e) {
